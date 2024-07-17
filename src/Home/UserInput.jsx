b29eb89e758855988990e027523d93b2216686b6
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-07-17 05:01:41 +0300

    updating the user input styling

--- src/Home/UserInput.jsx
+++ src/Home/UserInput.jsx
@@ -1,7 +1,17 @@
 import React from "react";
+import call from "../images/call-35.png";
 
 const UserInput = () => {
-  return <div className="user-input">UserInput</div>;
+  return (
+    <div className="user-input">
+      <div className="chat-info">
+        <span>Peter</span>
+        <div className="chat-icons">
+          <img src={call} alt="" />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default UserInput;
