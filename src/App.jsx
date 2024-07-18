545f74adbdcd97400c0a3e168de8618e9afedc15
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-07-18 06:04:49 +0300

    added page routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,22 @@
-import { useState } from "react";
+// import { useState } from "react";
 import HomePage from "./components/HomePage";
 import "./App.css";
 import { SignUpPage } from "./components/SignUpPage";
 import { LoginPage } from "./components/LoginPage";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
+  // const [count, setCount] = useState(0);
 
   return (
-    <>
-      <div>
-        {/* <HomePage /> */}
-        {/* <LoginPage /> */}
-        <SignUpPage />
-      </div>
-    </>
+    <BrowserRouter>
+      <Routes>
+        <Route index element={<HomePage />} />
+        <Route path="login" element={<LoginPage />} />
+        <Route path="signup" element={<SignUpPage />} />
+        {/* <SignUpPage /> */}
+      </Routes>
+    </BrowserRouter>
   );
 }
 
