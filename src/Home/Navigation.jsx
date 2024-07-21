c938ad36b4f61ecba05cca716c8e3730249d1faa
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-07-21 06:24:53 +0300

    updating to show current user

--- src/Home/Navigation.jsx
+++ src/Home/Navigation.jsx
@@ -1,18 +1,19 @@
-import React from "react";
-import user from "../images/videoframe_366.png";
-import { SignUpPage } from "../components/SignUpPage";
+import React, { useContext } from "react";
+// import user from "../images/videoframe_366.png";
+// import { SignUpPage } from "../components/SignUpPage";
 import { signOut } from "firebase/auth";
 import { auth } from "../firebase";
+import { Authentication } from "../context/Authentication";
 
 const Navigation = () => {
+  // updating user info
+  const { currentUser } = useContext(Authentication);
+
   return (
     <div className="navigation">
       <div className="user">
-        <img
-          src="https://i.pinimg.com/236x/5b/e8/bf/5be8bf498d97fd90dff4dd0ab78fb487.jpg"
-          alt=""
-        />
-        <span>Pteer</span>
+        <img src={currentUser.photoURL} alt="" />
+        <span>{currentUser.displayName}</span>
         <button onClick={() => signOut(auth)}>logout</button>
       </div>
       {/* <span className="logo">Chatty</span> */}
