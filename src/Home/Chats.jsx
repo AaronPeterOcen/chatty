c6ae2d0a45a86a83d7c83c277fdc0fb64536c66c
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-07-22 04:42:28 +0300

    updating user search bar functionality

--- src/Home/Chats.jsx
+++ src/Home/Chats.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import Navigation from "./Navigation";
 import SearchBar from "./SearchBar";
+import UserChat from "./UserChats";
 
 const Chats = () => {
   return (
     <div className="chats">
       <Navigation />
       <SearchBar />
+      <UserChat />
     </div>
   );
 };
--- src/Home/SearchBar.jsx
+++ src/Home/SearchBar.jsx
@@ -1,14 +1,52 @@
-import React from "react";
-import UserChat from "./UserChat";
+import React, { useState } from "react";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../firebase";
+// import UserChat from "./UserChats";
 
 const SearchBar = () => {
+  const [username, setUsername] = useState("");
+  const [user, setUser] = useState(null);
+  const [err, setErr] = useState(false);
+
+  const searchHandler = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username)
+    );
+
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (err) {
+      setErr(true);
+    }
+  };
+
+  const keyHandler = (e) => {
+    e.code === "Enter" && searchHandler();
+  };
+
   return (
     <div className="search">
       <div className="search-form">
-        <input type="text" placeholder="Search for User" />
-        <UserChat />
-        {/* <UserChat /> */}
+        <input
+          type="text"
+          placeholder="Search for User"
+          onKeyDown={keyHandler}
+          onChange={(e) => setUsername(e.target.value)}
+        />
       </div>
+      {err && <span>User not found!</span>}
+      {user && (
+        <div className="user-chats">
+          <img src={user.photoURL} alt="user" onClick={handleSelect} />
+          <div className="user-chat-info">
+            <span>{user.displayName}</span>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
