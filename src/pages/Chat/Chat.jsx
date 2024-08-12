9db01c24756a31d89eb513b79408b9c6c8ea0476
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-08-12 06:41:24 +0300

    working on the signUp/signIn page

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import "./Chat.css";
 
 const Chat = () => {
-  return <div>Chat</div>;
+  return <div className="chat"></div>;
 };
 
 export default Chat;
--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,8 +1,45 @@
 import React from "react";
 import "./SignIn.css";
+import chatLogo from "../../images/chat-sm.png";
 
 const SignIn = () => {
-  return <div>SignIn</div>;
+  return (
+    <div className="signIn">
+      <img src={chatLogo} className="logo" />
+      {/* adding the form field for user input */}
+      <form className="signIn-form">
+        <h2>Sign Up</h2>
+        <input
+          type="text"
+          placeholder="Username"
+          className="form-input"
+          required
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="form-input"
+          required
+        />
+        <input
+          type="password"
+          placeholder="password"
+          className="form-input"
+          required
+        />
+        <button>SignUp</button>
+        <div className="signIn-term">
+          <input type="checkbox" />
+          <p>Agree to all terms and conditions.</p>
+        </div>
+        <div className="signIn-forgot">
+          <p className="sigIn-toggle">
+            Already have an account with us? <span>Sign In here</span>
+          </p>
+        </div>
+      </form>
+    </div>
+  );
 };
 
 export default SignIn;
