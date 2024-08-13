9e9b955125f0490b4fc6d407e76c9717b94265d5
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-08-13 05:03:54 +0300

    updating the chat items and chat container styling

--- src/pages/Chat/Chat.jsx
+++ src/pages/Chat/Chat.jsx
@@ -1,11 +1,16 @@
 import React from "react";
 import "./Chat.css";
+import LeftSideBar from "../../components/LeftSideBar/LeftSideBar";
+import RightSideBar from "../../components/RightSideBar/RightSideBar";
+import ChatBox from "../../components/ChatBox/chatBox";
 
 const Chat = () => {
   return (
     <div className="chat">
       <div className="chat-container">
-        <rightSideBar />
+        <LeftSideBar />
+        <ChatBox />
+        <RightSideBar />
       </div>
     </div>
   );
