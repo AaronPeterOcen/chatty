bb46f3d3b0bb590fb5e3f4588870e34e3d6511f5
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-08-13 04:43:55 +0300

    using the useState to switch between the user signIn form fields

--- src/pages/SignIn/SignIn.jsx
+++ src/pages/SignIn/SignIn.jsx
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SignIn.css";
 import chatLogo from "../../images/chat-sm.png";
 
 const SignIn = () => {
+  // will be used to change the form state
+  const [currState, setCurrState] = useState("Sign Up");
+
   return (
     <div className="signIn">
       <img src={chatLogo} className="logo" />
       {/* adding the form field for user input */}
       <form className="signIn-form">
-        <h2>Sign Up</h2>
-        <input
-          type="text"
-          placeholder="Username"
-          className="form-input"
-          required
-        />
+        <h2>{currState}</h2>
+        {/* using currState to determine what will be displayed on the screen */}
+        {currState === "Sign Up" ? (
+          <input
+            type="text"
+            placeholder="Username"
+            className="form-input"
+            required
+          />
+        ) : null}
         <input
           type="email"
           placeholder="email"
@@ -27,15 +33,30 @@ const SignIn = () => {
           className="form-input"
           required
         />
-        <button>SignUp</button>
-        <div className="signIn-term">
-          <input type="checkbox" />
-          <p>Agree to all terms and conditions.</p>
-        </div>
+        <button>
+          {/* will change button state based on the user selected fields */}
+          {currState === "Sign Up" ? "Create account" : "Sign In "}
+        </button>
+        {currState === "Sign Up" ? (
+          <div className="signIn-term">
+            <input type="checkbox" />
+            <p>Agree to all terms and conditions.</p>
+          </div>
+        ) : null}
         <div className="signIn-forgot">
-          <p className="sigIn-toggle">
-            Already have an account with us? <span>Sign In here</span>
-          </p>
+          {currState === "Sign In" ? (
+            <p className="signIn-toggle">
+              Do not have an account with us?{" "}
+              {/* using setCurrState to change to the sign up form  */}
+              <span onClick={() => setCurrState("Sign Up")}> Sign Up here</span>
+            </p>
+          ) : (
+            <p className="signIn-toggle">
+              Already have an account with us?{" "}
+              {/* using setCurrState to switch to the sign in form  */}
+              <span onClick={() => setCurrState("Sign In")}> Sign In here</span>
+            </p>
+          )}
         </div>
       </form>
     </div>
