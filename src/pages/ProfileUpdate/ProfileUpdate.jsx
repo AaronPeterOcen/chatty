fe2d5af79b28273684a3bae6274d5922b2cf38c2
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-08-16 05:58:26 +0300

    completed the app ui for the profile

--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -4,7 +4,7 @@ import avatarUser from "../../images/user.png";
 import chatLogo from "../../images/chat-sm.png";
 
 const ProfileUpdate = () => {
-  // const [image , setImage] = useState(false);
+  const [image, setImage] = useState(false); // using state for determining the image state for the user
 
   return (
     <div className="profile">
@@ -13,20 +13,27 @@ const ProfileUpdate = () => {
           <h3>Profile Details</h3>
           <label htmlFor="avatar">
             <input
+              onChange={(e) => setImage(e.target.files[0])} // will allow the user to upload an img from files
               type="file"
               name=""
               id="avatar"
               accept=".png , .jpeg, .jpg"
               hidden
             />
-            <img className="" src={avatarUser} alt="" />
+            {/* Render the uploaded image if available, otherwise use the default
+            avatar */}
+            <img src={image ? URL.createObjectURL(image) : avatarUser} alt="" />
             Upload Image
           </label>
           <input type="text" placeholder="User name" required />
           <textarea placeholder="Bio" id="" required></textarea>
           <button type="submit">Save</button>
         </form>
-        <img className="profile-pic" src={chatLogo} alt="" />
+        <img
+          className="profile-pic"
+          src={image ? URL.createObjectURL(image) : chatLogo}
+          alt=""
+        />
       </div>
     </div>
   );
