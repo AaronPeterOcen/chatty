1c36d353612a6925804d3270d966738a30d222fe
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-08-21 12:22:30 +0300

    working on image upload and updating the user profile

---
+++ src/lib/upload.js
@@ -0,0 +1,51 @@
+import {
+  getStorage,
+  ref,
+  uploadBytesResumable,
+  getDownloadURL,
+} from "firebase/storage"; // Import Firebase Storage functions
+
+// Async function to handle file upload to Firebase Storage
+export default upload = async (file) => {
+  // Initialize Firebase Storage
+  const storage = getStorage();
+
+  // Create a reference to a location in Firebase Storage where the file will be saved
+  // Using a timestamp + file name to ensure a unique path
+  const storageRef = ref(storage, `images/${Date.now() + file.name}`);
+
+  // Start the file upload process using the Resumable Upload feature
+  const uploadTask = uploadBytesResumable(storageRef, file);
+
+  // Monitor the upload process
+  uploadTask.on(
+    "state_changed",
+    (snapshot) => {
+      // Calculate and log the upload progress
+      const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      console.log("Upload is " + progress + "% done");
+
+      // Log different states of the upload process
+      switch (snapshot.state) {
+        case "paused":
+          console.log("Upload is paused");
+          break;
+        case "running":
+          console.log("Upload is running");
+          break;
+      }
+    },
+    (error) => {
+      // Handle any errors during the upload process
+      console.error("Upload failed:", error);
+    },
+    () => {
+      // When the upload is complete, get the download URL of the uploaded file
+      getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        // Resolve the promise with the download URL
+        resolve(downloadURL);
+        console.log("File available at", downloadURL);
+      });
+    }
+  );
+};
--- src/pages/ProfileUpdate/ProfileUpdate.jsx
+++ src/pages/ProfileUpdate/ProfileUpdate.jsx
@@ -1,10 +1,54 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import "./ProfileUpdate.css";
 import avatarUser from "../../images/user.png";
 import chatLogo from "../../images/chat-sm.png";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth, db } from "../../config/firebase";
+import { doc, getDoc } from "firebase/firestore";
+import { useNavigate } from "react-router-dom";
 
 const ProfileUpdate = () => {
+  const navigate = useNavigate();
   const [image, setImage] = useState(false); // using state for determining the image state for the user
+
+  const [name, setName] = useState("");
+  const [bio, setBio] = useState("");
+  const [uid, setUid] = useState("");
+  const [prevImage, setPrevImage] = useState("");
+
+  useEffect(() => {
+    // Listen for changes in the authentication state
+    onAuthStateChanged(auth, async (user) => {
+      if (user) {
+        // If a user is authenticated, set the user's ID (uid) in state
+        setUid(user.uid);
+
+        // Reference to the user's document in the "users" collection
+        const docRef = doc(db, "users", user.uid);
+
+        // Fetch the user's document from the database
+        const docSnap = await getDoc(docRef);
+
+        // Check and set the user's name if it exists in the document
+        if (docSnap.data().name) {
+          setName(docSnap.data().name);
+        }
+
+        // Check and set the user's bio if it exists in the document
+        if (docSnap.data().bio) {
+          setBio(docSnap.data().bio);
+        }
+
+        // Check and set the user's avatar if it exists in the document
+        if (docSnap.data().avatar) {
+          setPrevImage(docSnap.data().avatar);
+        }
+      } else {
+        // If no user is authenticated, navigate to the home page
+        navigate("/");
+      }
+    });
+  });
 
   return (
     <div className="profile">
@@ -25,8 +69,20 @@ const ProfileUpdate = () => {
             <img src={image ? URL.createObjectURL(image) : avatarUser} alt="" />
             Upload Image
           </label>
-          <input type="text" placeholder="User name" required />
-          <textarea placeholder="Bio" id="" required></textarea>
+          <input
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+            type="text"
+            placeholder="User name"
+            required
+          />
+          <textarea
+            onChange={(e) => setBio(e.target.bio)}
+            value={bio}
+            placeholder="Bio"
+            id=""
+            required
+          ></textarea>
           <button type="submit">Save</button>
         </form>
         <img
