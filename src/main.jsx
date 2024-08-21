e96cadd853695fff3bd7e14e31df211ceab1b83b
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-08-21 08:04:41 +0300

    added context api to the main.jsx file

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.css";
 import { BrowserRouter } from "react-router-dom";
+import AppContextProvider from "./AppContext.jsx";
+// AppContextProvider
 // import { AuthContextProvider } from "./context/Authentication.jsx";
 // import { UserContextProvider } from "./context/UserContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
-    <App />
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
   </BrowserRouter>
 );
