e966065271a98d16ac96e9b2e465192cb729cdeb
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-09-02 11:23:50 +0300

    searching and getting users that are available in firebase databse

--- src/components/LeftSideBar/LeftSideBar.jsx
+++ src/components/LeftSideBar/LeftSideBar.jsx
@@ -1,13 +1,46 @@
-import React from "react";
+import React, { useContext, useState } from "react";
 import "./LeftSideBar.css";
 import chatLogo from "../../images/chat-sm.png";
 import menuIcon from "../../images/ellipsis.png";
 import searchIcon from "../../images/search.png";
 import userImage from "../../images/bird.jpg";
 import { useNavigate } from "react-router-dom";
+import { collection, getDocs, query, where } from "firebase/firestore";
+import { db } from "../../config/firebase";
+import { AppContext } from "../../AppContext";
 
 const LeftSideBar = () => {
   const navigate = useNavigate();
+  const { userData } = useContext(AppContext); // Extracting user data from AppContext using React's useContext hook.
+  const [user, setUser] = useState(null); // State to manage the user object found through search.
+  const [showSearch, setShowSearch] = useState(false); // State to manage visibility of the search results or suggestions.
+
+  const inputField = async (e) => {
+    try {
+      const input = e.target.value; // Getting the input value from the event's target.
+
+      if (input) {
+        setShowSearch(true); // Display search results if there is any input.
+
+        const userRef = collection(db, "users"); // Reference to the 'users' collection in Firestore.
+        const q = query(userRef, where("username", "==", input.toLowerCase())); // Create a Firestore query to find users with the username matching the input (case insensitive).
+
+        const querySnap = await getDocs(q); // Execute the query and get the documents (users) that match the query.
+
+        if (!querySnap.empty && querySnap.docs[0].data().id !== userData.id) {
+          // If there are results and the first user's id is not the same as the current user's id:
+          setUser(querySnap.docs[0].data()); // Set the found user data to the 'user' state.
+        } else {
+          setUser(null); // If no valid user is found, reset the 'user' state to null.
+        }
+      } else {
+        setShowSearch(false); // Hide search results if the input is empty.
+      }
+    } catch (error) {
+      // If an error occurs, you might want to handle it here (e.g., log the error, show a message to the user).
+    }
+  };
+
   return (
     <div className="ls">
       <div className="ls-top">
@@ -27,21 +60,32 @@ const LeftSideBar = () => {
         </div>
         <div className="ls-search">
           <img src={searchIcon} alt="" />
-          <input type="text" placeholder="Find a User..." />
+          <input
+            type="text"
+            onChange={inputField}
+            placeholder="Find a User..."
+          />
         </div>
       </div>
       <div className="ls-list">
-        {Array(10)
-          .fill("")
-          .map((item, index) => (
-            <div key={index} className="friends">
-              <img src={userImage} alt="" />
-              <div>
-                <p>Username</p>
-                <span>Hello there!</span>
+        {showSearch && user ? (
+          <div className="friends add-user">
+            <img src={user.avatar} alt="" />
+            <p>{user.name}</p>
+          </div>
+        ) : (
+          Array(10)
+            .fill("")
+            .map((item, index) => (
+              <div key={index} className="friends">
+                <img src={userImage} alt="" />
+                <div>
+                  <p>Username</p>
+                  <span>Hello there!</span>
+                </div>
               </div>
-            </div>
-          ))}
+            ))
+        )}
       </div>
     </div>
   );
